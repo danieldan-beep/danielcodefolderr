5aec4763621087f07a3d9af846f9f5e898f20787
danieldan-beep <daniel.oni@rcsd.ca>
2024-11-04 09:12:22 -0600

    fix

---
+++ project 5/src/App.java
@@ -0,0 +1,13 @@
+public class App  Turkeyrunner {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        system.out.println()
+        int myNum =2147483647
+   
+   
+   
+   
+    }
+
+}
+2c 30.0.0.0.0.0.0.0.0.
---
+++ project 5/src/Turkey.java
@@ -0,0 +1,3 @@
+public class Turkey {
+  public void speak
+}
